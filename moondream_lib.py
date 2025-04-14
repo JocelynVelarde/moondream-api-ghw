6e866e5588f2be7b33672cd760bd805741e50538
Jocelyn <a01285780@itesm.mx>
2025-04-13 21:06:53 -0600

    created two methods on the moondream class

---
+++ moondream_lib.py
@@ -0,0 +1,20 @@
+from typing import Union
+import moondream as md
+from PIL import Image
+
+class MoondreamHelper:
+    # Init moondream model
+    def __init__(self, api_key=None):
+        self.model = md.vl(api_key=api_key)
+
+    # Generate a description
+    def describe(self, image: Union[Image.Image, str], detail: str = "normal") -> str:
+        img = self._load_image(image)
+        caption = self.model.caption(img, length=detail)
+        return caption["caption"]
+    
+    # Query the image
+    def query(self, image: Union[Image.Image, str], question: str) -> str:
+        img = self._load_image(image)
+        query = self.model.query(img, question)
+        return query["answer"]
